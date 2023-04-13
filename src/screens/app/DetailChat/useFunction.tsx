e812aa161a7af380bf0ad3fdd3926f99fbb32e95
myan-software <80731323+myan-software@users.noreply.github.com>
2023-04-13 08:48:56 +0700

    コメントを追加

--- src/screens/app/DetailChat/useFunction.tsx
+++ src/screens/app/DetailChat/useFunction.tsx
@@ -435,6 +435,7 @@ export const useFunction = (props: any) => {
       setFormattedText(formattedText1);
       formattedText1.shift();
       setFormattedText(formattedText1);
+      // メッセージが送信完了の後、メッセージ入力のstateがemptyになる。
       setInputText('');
     },
     [messageReply, message_edit, ids, messageQuote],
