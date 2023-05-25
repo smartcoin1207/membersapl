18a88474a962d1fe79f5e144382f0f7bc79b9108
shoheihosotani <beclebecle@gmail.com>
2023-05-25 16:23:25 +0800

    socket通信の受信部分がまちがっていたのを修正

--- src/util/constanString.tsx
+++ src/util/constanString.tsx
@@ -7,13 +7,17 @@ export const EVENT_SOCKET = {
   //Sự kiện disconnect
   DISCONNECT: 'disconnect',
   //Sự kiện nhận message về (lắng nghe toàn bộ room nào cũng có thể nghe được)
+  // イベント受信メッセージ（すべてのルームを聞くことができます）
   NEW_MESSAGE_IND: 'new_message_ind',
   //Sự kiện dành cho việc realtime phần đã xem tin nhắn
+  //リアルタイム表示メッセージのイベント
   NEW_MESSAGE_CONF: 'new_message_conf',
   //Sự kiện realtime ngoài list chat (đổi tên, cập nhật các tin nhắn chưa đọc, đổi thông tin phòng ...)
-  CHAT_GROUP_UPDATE_IND: 'ChatGroup_update_ind2',
+  //チャットリスト外のリアルタイムイベント（名前変更、未読メッセージの更新、ルーム情報の変更...）
+  CHAT_GROUP_UPDATE_IND: 'ChatGroup_update_ind',
   //Sự kiện nhận message về (chỉ nghe được khi đã join vào 1 room)
-  MESSAGE_IND: 'message_ind2',
+  // イベント受信メッセージ (ルームに参加したときにのみ聞こえます)
+  MESSAGE_IND: 'message_ind',
 };
 
 export const ATTACHMENT_FILE_TYPE = {
