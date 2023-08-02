5cb8775604e276c424cd11d5275cbdfa8ef86bf8
shoheihosotani <beclebecle@gmail.com>
2023-08-02 14:14:30 +0800

    AHD-9821 アプリ版：API連携でチャット投稿した際に \n がアプリ版のみ改行が出来ていない

--- src/screens/app/DetailChat/components/MessageInfo.tsx
+++ src/screens/app/DetailChat/components/MessageInfo.tsx
@@ -150,7 +150,7 @@ export default function MessageInfo({
         defaultTextProps={textSetting}
         contentWidth={width}
         source={{
-          html: text ? convertMentionToLink(customAnchorify(convertMessageNotation(text.split('\n').join('<br>'))), joinedUsers) : ''
+          html: text ? convertMentionToLink(customAnchorify(convertMessageNotation(text.replace(/\n/g, '<br>'))), joinedUsers) : ''
         }}
         customHTMLElementModels={customHTMLElementModels}
         // @ts-ignore
