4cdaa949b005c4a9b0222bae817093aa477d9385
shoheihosotani <beclebecle@gmail.com>
2023-11-21 09:29:00 +0800

    AHD-11113 アプリ版:１：１のグループチャットを開くと、チャットグループ名が自分の名前になっている場合がある

--- src/screens/app/DetailChat/index.tsx
+++ src/screens/app/DetailChat/index.tsx
@@ -233,7 +233,7 @@ const DetailChat = (props: any) => {
                     : ''
                 } ${
                   dataDetail?.one_one_check &&
-                  dataDetail?.one_one_check[0]?.length > 0
+                  dataDetail?.one_one_check?.length > 0
                     ? dataDetail?.one_one_check[0]?.first_name
                     : ''
                 }`
