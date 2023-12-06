cd789f57b232801abef8cd00bd2dfa423f32eeca
shoheihosotani <beclebecle@gmail.com>
2023-12-06 18:52:30 +0800

    AHD-11257 アプリ版：web版から送信されたシステムメッセージが正しく表示されない

--- src/screens/app/DetailChat/components/ItemMessage.tsx
+++ src/screens/app/DetailChat/components/ItemMessage.tsx
@@ -470,7 +470,6 @@ const ItemMessage = React.memo((props: any) => {
                       )}
                       {msg_type == 6 || msg_type == 8 || msg_type == 14 ? null : (
                         <View style={user?._id == user_id ? [styles.containerViewChat, {alignItems: 'flex-end',}] : [styles.containerViewChat, {alignItems: 'flex-start',}]}>
-                          <Text>shohei</Text>
                           {reply_to_message_text ||
                           reply_to_message_files?.length > 0 ||
                           reply_to_message_stamp?.stamp_icon ||
@@ -609,7 +608,6 @@ const ItemMessage = React.memo((props: any) => {
                 msg_type == 8 ||
                 msg_type == 14 ? null : (
                   <>
-                    <Text>shohei</Text>
                     {moment(createdAt) < moment(updated_at) ? (
                       <Text style={styles.txtTime}>
                         {moment(updated_at, 'YYYY/MM/DD hh:mm:ss').format(
