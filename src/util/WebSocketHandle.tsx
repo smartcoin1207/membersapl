6ceef0ceb185469a4e8d69bbeeef2450b9177c2a
Akira.K <akira_kusano@aid-hd.jp>
2024-03-21 13:35:40 +0900

    レビュー対応。websocketイベントをログ出力する様に修正。

--- src/util/WebSocketHandle.tsx
+++ src/util/WebSocketHandle.tsx
@@ -149,6 +149,7 @@ function createAppSocket() {
     //AHD-11819用修正。
     //web側から詳細チャットを開いた段階でアプリ側に送信されるのがこれのみのため、暫定的にルームの既読状態を検知するために使用。
     socket.on(EVENT_SOCKET.CHANGE_BROWSER_ICON, (data: any) => {
+      console.log("CHANGE_BROWSER_ICON",data);
       const state = store.getState();
 
       if(data.user_id === state.auth.userInfo.id && state.chat.roomList.length > 0 && data.unread_count > 0) {
