d90fb6e0713ababa632d405054544e8cce67984e
Anh Nguyen <anh.nguyen@pionero.com>
2024-06-12 08:14:46 +0700

    fix: remame ref as ~Ref

--- src/screens/app/DetailChat/components/Composer.tsx
+++ src/screens/app/DetailChat/components/Composer.tsx
@@ -1,9 +1,9 @@
 import React, {
   type Ref,
-  forwardRef,
   useImperativeHandle,
   useRef,
   useState,
+  forwardRef,
 } from 'react';
 import {StyleSheet, TextInput, type NativeSyntheticEvent} from 'react-native';
 import type {ComposerProps} from '../../../../lib/react-native-gifted-chat/lib';
@@ -13,79 +13,81 @@ export type ComposerRef = {
   onUnFocus: () => void;
 };
 
-const Composer = (
-  {
-    placeholderTextColor = Color.defaultColor,
-    textInputProps = {},
-    onTextChanged,
-    onInputSizeChanged,
-    disableComposer = false,
-    textInputStyle = {},
-    textInputAutoFocus = false,
-    keyboardAppearance = 'default',
-  }: ComposerProps,
-  ref: Ref<ComposerRef> | undefined,
-) => {
-  const refInput = useRef<any>();
+const Composer = forwardRef<ComposerRef, ComposerProps>(
+  (
+    {
+      placeholderTextColor = Color.defaultColor,
+      textInputProps = {},
+      onTextChanged,
+      onInputSizeChanged,
+      disableComposer = false,
+      textInputStyle = {},
+      textInputAutoFocus = false,
+      keyboardAppearance = 'default',
+    }: ComposerProps,
+    ref: Ref<ComposerRef> | undefined,
+  ) => {
+    const inputRef = useRef<any>();
 
-  useImperativeHandle(ref, () => ({
-    onUnFocus,
-  }));
+    useImperativeHandle(ref, () => ({
+      onUnFocus,
+    }));
 
-  const [currentContentSize, setCurrentContentSize] = useState<
-    {width: number; height: number} | undefined
-  >();
+    const [currentContentSize, setCurrentContentSize] = useState<
+      {width: number; height: number} | undefined
+    >();
 
-  const handleContentSizeChange = (e: NativeSyntheticEvent<any>) => {
-    const {contentSize} = e.nativeEvent;
+    const handleContentSizeChange = (e: NativeSyntheticEvent<any>) => {
+      const {contentSize} = e.nativeEvent;
 
-    // Support earlier versions of React Native on Android
-    if (!contentSize) {
-      return;
-    }
+      // Support earlier versions of React Native on Android
+      if (!contentSize) {
+        return;
+      }
 
-    if (
-      !currentContentSize ||
-      (currentContentSize &&
-        (currentContentSize.width !== contentSize.width ||
-          currentContentSize.height !== contentSize.height))
-    ) {
-      setCurrentContentSize(contentSize);
-      onInputSizeChanged?.(contentSize);
-    }
-  };
+      if (
+        !currentContentSize ||
+        (currentContentSize &&
+          (currentContentSize.width !== contentSize.width ||
+            currentContentSize.height !== contentSize.height))
+      ) {
+        setCurrentContentSize(contentSize);
+        onInputSizeChanged?.(contentSize);
+      }
+    };
 
-  const handleChangeText = (textChange: string) => {
-    onTextChanged?.(textChange);
-  };
+    const handleChangeText = (textChange: string) => {
+      onTextChanged?.(textChange);
+    };
 
-  const onUnFocus = () => {
-    refInput.current.blur();
-  };
+    const onUnFocus = () => {
+      inputRef.current.blur();
+    };
 
-  return (
-    <TextInput
-      accessible
-      accessibilityLabel="メッセージ"
-      placeholder="メッセージ"
-      placeholderTextColor={placeholderTextColor}
-      multiline
-      editable={!disableComposer}
-      onChange={handleContentSizeChange}
-      onContentSizeChange={handleContentSizeChange}
-      onChangeText={handleChangeText}
-      style={[styles.textInput, textInputStyle]}
-      autoFocus={textInputAutoFocus}
-      enablesReturnKeyAutomatically
-      underlineColorAndroid="transparent"
-      keyboardAppearance={keyboardAppearance}
-      textAlignVertical="center"
-      value={undefined}
-      {...textInputProps}
-      ref={refInput}
-    />
-  );
-};
+    return (
+      <TextInput
+        accessible
+        accessibilityLabel="メッセージ"
+        placeholder="メッセージ"
+        placeholderTextColor={placeholderTextColor}
+        multiline
+        editable={!disableComposer}
+        onChange={handleContentSizeChange}
+        onContentSizeChange={handleContentSizeChange}
+        onChangeText={handleChangeText}
+        style={[styles.textInput, textInputStyle]}
+        autoFocus={textInputAutoFocus}
+        enablesReturnKeyAutomatically
+        underlineColorAndroid="transparent"
+        keyboardAppearance={keyboardAppearance}
+        textAlignVertical="center"
+        value={undefined}
+        {...textInputProps}
+        ref={inputRef}
+      />
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   textInput: {
@@ -94,4 +96,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default forwardRef(Composer);
+export default Composer;
