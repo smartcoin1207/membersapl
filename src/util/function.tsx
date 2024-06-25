883f85d7d5848f8b572cb6bbe95506809d3c4868
“ytachibana0511” <tachi84g@gmail.com>
2024-06-25 17:06:31 +0900

    fix

--- src/util/function.tsx
+++ src/util/function.tsx
@@ -45,7 +45,6 @@ export const convertString = (str: any = '') => {
       rv[`${item}`] = '';
     });
   }
-
   text = replaceAll(str, rv) || '';
   text = text.replace(/\[(\/*)(title|info|hr|bold|red)\]/g, '');
   text = text.replace(/<br>/g, '\n');
